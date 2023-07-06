29cfb52a74eb19b7885ce07fed71d08578c35c4a
MikePowar <mike.powar@gmail.com>
2023-07-05 17:25:17 -0700

    Initial Commit

---
+++ SplitTheBill/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SplitTheBill
+//
+//  Created by Mike Powar on 2023-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitTheBill/SplitTheBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTheBillApp.swift
+//  SplitTheBill
+//
+//  Created by Mike Powar on 2023-07-05.
+//
+
+import SwiftUI
+
+@main
+struct SplitTheBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
