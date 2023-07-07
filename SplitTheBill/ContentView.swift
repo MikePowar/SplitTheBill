0dfe4ff11d9639b944dec3335fb2c9b576890de6
MikePowar <mike.powar@gmail.com>
2023-07-06 17:52:39 -0700

    Day 18: Challenges - WeSplit

--- SplitTheBill/ContentView.swift
+++ SplitTheBill/ContentView.swift
@@ -18,30 +18,37 @@ struct ContentView: View {
     // Focus on Amount to assist with keyPad disappearing when not needed
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
+    // let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
         // calculate total per person here
         
         // people count - cast to Double
         let peopleCount = Double(numberOfPeople + 2)
-        // tip Selection - cast to double
-        let tipSelection = Double(tipPercentage)
         
-        //Then calculate - tipValue, Grand Total, and amountPerPerson
-        let tipValue = checkAmount  / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
+        //Then calculate - amountPerPerson
         let amountPerPerson = grandTotal / peopleCount
              
         return amountPerPerson
     }
     
+    // Challenge 2: Add another section
+    // - total amount for the check (original amount + tip value, w/o dividing by # of people)
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    let localCurrency: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "USD")
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format:
-                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    TextField("Amount", value: $checkAmount, format: localCurrency)
                     .keyboardType(.decimalPad)
                     .focused($amountIsFocused)
                     
@@ -54,18 +61,27 @@ struct ContentView: View {
                 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson, format: localCurrency)
+                } header: {
+                Text("Amount Per Person")
                 }
+                // Challenge 2: Add another section
+                // - total amount for the check (original amount + tip value, w/o dividing by # of people)
+                Section {
+                    Text(grandTotal, format: localCurrency)
+                } header: {
+                    Text("Grand Total")
+                }
+                
             }
             .navigationTitle("WeSplit")
             .toolbar {
